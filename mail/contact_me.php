c8430f3748bead260d6a37ae85f990020cb2d75f
Luciano Cassettai <lcassettai>
2022-09-20 20:17:26 -0300

    Inicio

---
+++ mail/contact_me.php
@@ -0,0 +1,26 @@
+<?php
+// check if fields passed are empty
+if(empty($_POST['name'])  		||
+   empty($_POST['email']) 		||
+   empty($_POST['phone']) 		||
+   empty($_POST['message'])	||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+	echo "No arguments Provided!";
+	return false;
+   }
+	
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+	
+// create email body and send it	
+$to = 'luciano.cassettai@gmail.com'; 
+$email_subject = "Consulta :  $name";
+$email_body = "Recibiste un nuevo mensaje desde tu portfolio. \n\n"."Estos son los detalles:\n\nNombre: $name\n\nEmail: $email_address\n\nTelefono: $phone\n\nMensaje:\n$message";
+$headers = "From: luciano.cassettai@gmail.com\n"; // *REPLACE WITH THE EMAIL ADDRESS YOU WANT THE MESSAGE TO BE FROM*
+$headers .= "Reply-To: $email_address";	
+mail($to,$email_subject,$email_body,$headers);
+return true;			
+?>
